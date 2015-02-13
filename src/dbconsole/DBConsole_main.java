86ff36f007dc348d630fe5b14bb1dff2f70fdf5e
50316025 <sumiyamashin@yahoo.co.jp>
2015-02-13 14:06:30 +0900

    20150213kadai

---
+++ src/dbconsole/DBConsole_main.java
@@ -0,0 +1,16 @@
+package dbconsole;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConsole_main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Console_View graph=new Console_View();
+graph.setBounds(5,5,655,455);
+graph.setVisible(true);
+	}
+}
